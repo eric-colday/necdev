f418a08424ebe52ce1710b6f6b0056aa922a5540
Éric  Colday NOUBISSI <54113095+eric-colday@users.noreply.github.com>
2024-11-15 15:39:21 +0100

    reduction taille de texte

--- src/components/transitionProvider.jsx
+++ src/components/transitionProvider.jsx
@@ -23,7 +23,7 @@ const TransitionProvider = ({ children }) => {
           transition={{ duration: 0.5, ease: "easeOut" }}
         />
         <motion.div
-          className="fixed m-auto top-0 bottom-0 left-0 right-0 text-white text-8xl cursor-default z-50 w-fit h-fit capitalize"
+          className="fixed m-auto top-0 bottom-0 left-0 right-0 text-white text-8xl max-[500px]:text-5xl cursor-default z-50 w-fit h-fit capitalize"
           initial={{ opacity: 1 }}
           animate={{ opacity: 0 }}
           exit={{ opacity: 0 }}
